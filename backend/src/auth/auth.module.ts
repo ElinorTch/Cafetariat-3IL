8cfd5d87814501f7ca34d3fd586d356a8bc5af4d
ElinorTch <tchamoelii@gmail.com>
2024-09-20 00:37:53 +0200

    Feat: authentication guard API

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,12 +4,27 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/database/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constant';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '3600s' },
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    AuthService,
+  ],
 })
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,19 +1,58 @@
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../database/entities/user.entity';
 import { UserDto } from '../database/dto/user.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
 
-  async create(userDto: UserDto): Promise<User> {
+  async signUp(userDto: UserDto): Promise<{ access_token: string }> {
     const user = new this.userModel(userDto);
-    return user.save();
+    const saltOrRounds = 10;
+    const hash = await bcrypt.hash(userDto.password, saltOrRounds);
+    user.password = hash;
+    user.save();
+
+    const payload = { sub: user._id, email: user.email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+
+  async signIn(userDto: UserDto): Promise<{ access_token: string }> {
+    const user = await this.getByEMail(userDto.email);
+    const isMatch = await bcrypt.compare(userDto.password, user.password);
+    if (!isMatch) throw new UnauthorizedException();
+    const payload = { sub: user._id, email: user.email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec();
+  }
+
+  async getById(id: string): Promise<User> {
+    const user = this.userModel.findById({ id });
+    if (!user) throw new BadRequestException("This user doesn't exist.");
+    return user;
+  }
+
+  async getByEMail(email: string): Promise<User> {
+    const user = await this.userModel.findOne({ email: email });
+    if (!user) throw new BadRequestException("This user doesn't exist.");
+    return user;
   }
 }

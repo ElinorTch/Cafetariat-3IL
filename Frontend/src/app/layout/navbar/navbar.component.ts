3e19bcc416592d4a906a11c6b30344eef33bc61a
Heyppy <maggioe@3il.fr>
2024-10-15 14:55:11 +0200

    Vaguelette et Bouton

--- Frontend/src/app/layout/navbar/navbar.component.ts
+++ Frontend/src/app/layout/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { hugeClipboard, hugeProductLoading } from '@ng-icons/huge-icons';
 import { AuthService } from '../../auth/data-access/auth.service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -18,13 +19,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent{
   user: any;
+  showButton: boolean = false;
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private router : Router) {}
 
   ngOnInit() {
     this.getUser();
+    this.router.events.subscribe((val) => {
+      this.showButton = this.router.url.startsWith('/home/');
+    });
   }
 
   private getUser(): void {
